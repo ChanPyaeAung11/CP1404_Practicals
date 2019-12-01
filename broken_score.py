a0a9b0fde46ff2513ea31a34ffc0b3b237314894
ChanPyaeAung11 <chanpyaeaung@my.jcu.edu.au>
2019-12-01 22:31:36 +0800

    original ones

--- broken_score.py
+++ broken_score.py
@@ -1,18 +1,14 @@
-MENU = "MENU \n C - Convert Celsius to Fahrenheit \n F - Convert Fahrenheit to Celsius \n Q - Quit"
+def main():
 
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "C":
-        ask_celsius = float(input("Celsius: "))
-        calc_fahrenheit = ask_celsius * 9 / 5 + 32
-        print("Result: {:.2f} F".format(calc_fahrenheit))
-    elif choice == "F":
-        ask_fahrenheit = float(input("Fahrenheit: "))
-        calc_celsius = 5/9 * (ask_fahrenheit - 32)
-        print("Result: {:.2f} C ".format(calc_celsius))
+    score = float(input("Enter score: "))
+    if 0 > score or score > 100:
+        print("Invalid score")
+    elif 50 <= score <= 90:
+        print("Passable")
+    elif score > 90:
+        print("Excellent")
     else:
-        print("Invalid option")
-    print(MENU)
-    choice = input(">>> ").upper()
-print("Thank you.")
+        print("Bad")
+
+
+main()
