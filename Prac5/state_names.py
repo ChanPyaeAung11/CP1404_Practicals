14759a062e4741c86849116dc045775f95d60f1a
ChanPyaeAung11 <chanpyaeaung@my.jcu.edu.au>
2019-12-13 18:01:40 +0800

    Update state_names.py
    user input needs to use .upper().
    
    prints out all the states

--- Prac5/state_names.py
+++ Prac5/state_names.py
@@ -7,11 +7,13 @@ File needs reformatting
 STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 # print(STATE_NAMES)
-
-state = input("Enter short state: ").lower()
+for i in STATE_NAMES:
+    print(i, "is", STATE_NAMES[i])
+state = input("Enter short state: ").upper()
 while state != "":
     if state in STATE_NAMES:
         print(state, "is", STATE_NAMES[state])
     else:
         print("Invalid short state")
-    state = input("Enter short state: ")
+    state = input("Enter short state: ").upper()
+print(STATE_NAMES["QLD"])
