6e3e5dae84cb6543474a5510ad7f42ab49e5566c
ChanPyaeAung11 <chanpyaeaung@my.jcu.edu.au>
2019-12-13 09:50:01 +0800

    Update list_exercises.py
    make fucntion to check user names and report back access denied or granted.
    
    make two seperate functions

--- Prac4/list_exercises.py
+++ Prac4/list_exercises.py
@@ -1,13 +1,31 @@
-numbers = []
-for i in range(5):
-    number = int(input("Number:"))
-    numbers.append(number)
+def main():
+    numbers = []
+    for i in range(5):
+        number = int(input("Number:"))
+        numbers.append(number)
+    listoperations(numbers)
+    name_checker()
 
-for j in numbers:
-    print("Number:",j)
+def listoperations(numbers):
+    for j in numbers:
+        print("Number:",j)
 
-print("The first number is", numbers[0])
-print("The last number is", numbers[-1])
-print("The smallest number is", min(numbers))
-print("The largest number is", max(numbers))
-print("The average number is", sum(numbers)/len(numbers))
+    print("The first number is", numbers[0])
+    print("The last number is", numbers[-1])
+    print("The smallest number is", min(numbers))
+    print("The largest number is", max(numbers))
+    print("The average number is", sum(numbers)/len(numbers))
+
+
+def name_checker():
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+    askName = input("Username:")
+    if askName in usernames:
+        print("access granted")
+    else:
+        print("Access denied")
+
+
+main()
