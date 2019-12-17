da51652e1c5f1f89e78caa54467b3c0d77fab553
ChanPyaeAung11 <chanpyaeaung@my.jcu.edu.au>
2019-12-17 16:47:08 +0800

    Update hex_colours.py

--- Prac5/hex_colours.py
+++ Prac5/hex_colours.py
@@ -1,9 +1,13 @@
-HEX_DICT = {"alice blue": "#f0f8ff", "azure": ["#f0ffff", "#e0eeee", "#c1cdcd", "#838b8b"], "beige": "#f5f5dc", "ghost white": "#f8f8ff",
-            "light": "#eedd82"}
+HEX_DICT = {"alice blue": "#f0f8ff", "azure": ["#f0ffff", "#e0eeee",
+            "#c1cdcd", "#838b8b"], "beige": "#f5f5dc", "ghost white": "#f8f8ff",
+            "light": "#eedd82", "light gray": "#d3d3d3", "linen": "#faf0e6",
+            "medium": "#66cdaa", "medium aquamarine": "#66cdaa",
+            "mint cream": "#f5fffa", "red": "#ff0000", "salmon": "#fa8072",
+            "sandy brown": "#f4a460"}
 
 hex_color = input("Color Name: ").lower()
 while hex_color != "":
-    if hex_color in HEX_DICT:
+    if hex_color in HEX_DICT:  
         print(HEX_DICT[hex_color])
     else:
         hex_color = input("Color Name: ").lower()
