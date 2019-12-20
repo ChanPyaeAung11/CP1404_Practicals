52ff19c70694f9b9a6a9ed9d883417ba25cebe17
ChanPyaeAung11 <chanpyaeaung@my.jcu.edu.au>
2019-12-20 17:52:22 +0800

    Create languages.py
    import ProgrammiongLanguage class and use here

---
+++ Prac6/languages.py
@@ -0,0 +1,13 @@
+from programming_language import ProgrammingLanguage
+
+
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+program_list = [ruby, python, visual_basic]
+print("The dynamically typede languages are \n",)
+for i in program_list:
+    if i.is_dynamic():
+        print(i.name)
+
