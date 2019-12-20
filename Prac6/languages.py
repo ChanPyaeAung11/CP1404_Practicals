3c56a185d089d779dc57230b88ccda188347d67a
ChanPyaeAung11 <chanpyaeaung@my.jcu.edu.au>
2019-12-20 18:12:36 +0800

    Update languages.py
    put a main function

--- Prac6/languages.py
+++ Prac6/languages.py
@@ -2,12 +2,18 @@
 from Prac6.programming_language import ProgrammingLanguage
 
 
-ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+def main():
+    """ start of program """
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 
-program_list = [ruby, python, visual_basic]
-print("The dynamically typed languages are \n",)
-for i in program_list:
-    if i.is_dynamic():
-        print(i.name)
+    languages = [ruby, python, visual_basic]
+    print("The dynamically typed languages are \n", )
+    for i in languages:
+        if i.is_dynamic():
+            print(i.name)
+
+
+if __name__ == "__main__":
+    main()
