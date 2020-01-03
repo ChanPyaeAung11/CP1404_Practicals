021a6d9b23dec0298db0922cb01c76c85fb67c8e
ChanPyaeAung11 <chanpyaeaung@my.jcu.edu.au>
2020-01-03 18:36:34 +0800

    output has exception handling to handle floats and strings.
    output color is pink

--- Prac7/squaring.py
+++ Prac7/squaring.py
@@ -14,17 +14,23 @@ __author__ = 'Lindsay Ward'
 
 class SquareNumberApp(App):
     """ SquareNumberApp is a Kivy App for squaring a number """
+
     def build(self):
         """ build the Kivy app from the kv file """
-        Window.size = (200, 100)
+        Window.size = (400, 200)
         self.title = "Square Number"
         self.root = Builder.load_file('squaring.kv')
         return self.root
 
     def handle_calculate(self, value):
         """ handle calculation (could be button press or other call), output result to label widget """
-        result = value ** 2
-        self.root.ids.output_label.text = str(result)
+        try:
+            result = float(value)
+            ans = result ** 2
+            self.root.ids.output_label.text = str(ans)
+
+        except ValueError:
+            self.root.ids.output_label.text = str("a number plz")
 
 
 SquareNumberApp().run()
